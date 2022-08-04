67dd9a22313caa784f39a25e09c969321930cc0e
dbckd999 <dbckd999@naver.com>
2022-08-04 22:07:31 +0900

    test

---
+++ src/main/java/com/shoping/dbckd/DbckdApplication.java
@@ -0,0 +1,13 @@
+package com.shoping.dbckd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbckdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbckdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoping/dbckd/DbckdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoping.dbckd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbckdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
