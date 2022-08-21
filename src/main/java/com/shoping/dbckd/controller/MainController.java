79e6825f74f2069f974142df60e01bf4e0baa4fa
dbckd999 <dbckd999@naver.com>
2022-08-21 22:55:06 +0900

    index 페이지 오류 수정

--- src/main/java/com/shoping/dbckd/controller/MainController.java
+++ src/main/java/com/shoping/dbckd/controller/MainController.java
@@ -1,23 +1,12 @@
 package com.shoping.dbckd.controller;
 
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.sql.rowset.serial.SerialBlob;
-import javax.sql.rowset.serial.SerialException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.mysql.cj.jdbc.Blob;
