6561fe033da0953916db36ab5bd2f33293dab70a
dbckd999 <dbckd999@naver.com>
2022-08-25 00:15:45 +0900

    로그인 로직 분활

--- src/main/java/com/shoping/dbckd/controller/MainController.java
+++ src/main/java/com/shoping/dbckd/controller/MainController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,9 +12,13 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.mysql.cj.jdbc.Blob;
 import com.shoping.dbckd.mapper.MainMapper;
+import com.shoping.dbckd.model.CustomerDTO;
 import com.shoping.dbckd.service.MainService;
 
 @Controller
+/**
+ * 전반적인 페이지를 나타냅니다.
+ */
 public class MainController {
 
 	@Autowired
@@ -23,20 +28,23 @@ public class MainController {
 	MainMapper mapper;
 
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
 		// 아래 주석을 해제하면 내 집에있는 db서버에서 데이터를 가져온다.
 		// 너무 많이 새로고침만 하지 마셈.... 파이로 켜놓는거라 자원이 적다
-		// System.out.println(mapper.test(1));
+		System.out.println(mapper.test(1));
+
+		CustomerDTO dto = mapper.test(1);
+
+		model.addAttribute("var123", dto.getNick());
+
 		return "index";
 	}
-	@GetMapping("/sign")
-	public String sign() {
-		return "sign";
+
+	@GetMapping("/index")
+	public String returnindex() {
+		return "index";
 	}
-	@GetMapping("/sign_up")
-	public String sign_up() {
-		return "sign_up";
-	}
+
 	@GetMapping("/uploaditem")
 	public String uploaditem() {
 		return "uploaditem";
@@ -49,18 +57,8 @@ public class MainController {
 	public String view() {
 		return "view";
 	}
-	@GetMapping("/index")
-	public String returnindex() {
-		return "index";
-	}
-	@GetMapping("/find_id")
-	public String find_id() {
-		return "find_id";
-	}
-	@GetMapping("/find_pw")
-	public String find_pw() {
-		return "find_pw";
-	}
+
+
 	@PostMapping("/fileTest")
 	public String blobTest(@RequestParam("file") MultipartFile file) throws IOException {
 		System.out.println("test...");
---
+++ src/main/java/com/shoping/dbckd/controller/MemberController.java
@@ -0,0 +1,43 @@
+package com.shoping.dbckd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller(value = "/sign")
+/**
+ * 회원이 사용하는 페이지 입니다.
+ */
+public class MemberController {
+    
+    @GetMapping("sign")
+	public String sign() {
+        return "sign";
+	}
+    
+	@GetMapping("sign_up")
+	public String sign_up() {
+        System.out.println("aaa");
+		return "sign_up";
+	}
+
+    @GetMapping("find_id")
+	public String find_id() {
+		return "find_id";
+	}
+
+	@GetMapping("find_pw")
+	public String find_pw() {
+		return "find_pw";
+	}
+
+    @GetMapping(value = "sign_up2")
+    public String sign_up_test(@RequestParam String id2){
+        System.out.println("id2: " + id2);
+
+        System.out.println("회원가입 완료.");
+        return "/";
+    }
+
+}
